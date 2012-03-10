3a20886042f2d5f971fa332c2be73d14292319aa
Jason Smith (work) <jhs@iriscouch.com>
2012-03-10 08:31:44 +0000

    Basic API tests passing

--- src/request.erl
+++ src/request.erl
@@ -11,26 +11,95 @@
 % the License.
 
 -module(request).
--export([api/0]).
+-export([start/0, start/1, api/0, api/1]).
 -export([req/1, get/1, put/1, post/1, delete/1]).
-%-export([req/2, get/2, put/2, post/2, delete/2]).
+-export([req/2, get/2, put/2, post/2, delete/2]).
 
-api() -> fun req/1.
-
-%req(Uri, Callback) when is_list(Uri) andalso is_function(Callback) -> ok
-req(Url) -> ok
+start() -> ok
+    , ssl:start()
+    , inets:start()
     .
 
-get(Uri) -> ok
+start(Type) -> ok
+    , ssl:start(Type)
+    , inets:start(Type)
     .
 
-put(Uri) -> ok
+api() -> ok
+    , start()
+    , fun ?MODULE:req/1
     .
 
-post(Uri) -> ok
+api(async) -> ok
+    , start()
+    , fun ?MODULE:req/2
     .
 
-delete(Uri) -> ok
+req(Url, Callback) when is_list(Url) -> ok
+    , req({[{url,Url}]}, Callback)
+    ;
+
+req({Options}, Callback) when is_list(Options) andalso is_function(Callback) -> ok
+    , Request_pid = spawn(fun() -> waiter(Options, Callback) end)
+    , Request_pid
+    .
+
+req(Url) when is_list(Url) -> ok
+    , req({[{url, Url}]})
+    ;
+
+req({Options}) when is_list(Options) -> ok
+    , Uri = oget(Options, uri)
+    , Url = oget(Options, url, Uri)
+    , case Url
+        of undefined -> ok
+            , throw({bad_arg, "Missing url or uri option"})
+        ; _ -> ok
+            , case httpc:request(Url)
+                of {error, Reason} -> ok
+                    , {error, Reason}
+                ; {ok, Result} -> ok
+                    , {Result, <<"I am the body">>}
+                end
+        end
+    .
+
+get(Options) -> ok
+    , req(oset(Options, method, 'GET'))
+    .
+
+put(Options) -> ok
+    , req(oset(Options, method, 'PUT'))
+    .
+
+post(Options) -> ok
+    , req(oset(Options, method, 'POST'))
+    .
+
+delete(Options) -> ok
+    , req(oset(Options, method, 'DELETE'))
+    .
+
+get(Options, Callback) -> ok
+    , req(oset(Options, method, 'GET'), Callback)
+    .
+
+put(Options, Callback) -> ok
+    , req(oset(Options, method, 'PUT'), Callback)
+    .
+
+post(Options, Callback) -> ok
+    , req(oset(Options, method, 'POST'), Callback)
+    .
+
+delete(Options, Callback) -> ok
+    , req(oset(Options, method, 'DELETE'), Callback)
+    .
+
+
+waiter(Options, Callback) -> ok
+    , {One, Two} = req({Options})
+    , Callback(One, Two)
     .
 
 %
@@ -43,5 +112,41 @@ as_binary(A) when is_atom(A) ->
     list_to_binary(atom_to_list(A));
 as_binary(L) when is_list(L) ->
     list_to_binary(L).
+
+odel(Obj, Key) -> ok
+    , oset(Obj, Key, undefined)
+    .
+
+oset({Obj}, Key, Value) -> ok
+    , oset(Obj, Key, Value)
+    ;
+
+oset(Obj, Key, undefined) -> ok
+    , lists:keydelete(Key, 1, Obj)
+    ;
+
+oset(Obj, Key, Value) -> ok
+    , lists:keystore(Key, 1, Obj, {Key, Value})
+    .
+
+oget(Obj, Key) -> ok
+    , oget(Obj, Key, undefined)
+    .
+
+oget({Obj}, Key, Default) -> ok
+    , oget(Obj, Key, Default)
+    ;
+
+oget(Obj, Key, Default) when is_list(Obj) -> ok
+    , case lists:keyfind(Key, 1, Obj)
+        of {Key, Value} -> ok
+            , Value
+        ; false -> ok
+            , case is_atom(Key)
+                of true -> oget(Obj, as_binary(Key), Default)
+                ; false -> Default
+                end
+        end
+    .
 
 % vim: sts=4 sw=4 et
--- test/002-api.t
+++ test/002-api.t
@@ -1,18 +1,60 @@
 #! /usr/bin/env escript
 
+-define(COUCH, "http://localhost:5984").
+-define(TIMEOUT, 3000).
+
 main([]) -> ok
     , code:add_pathz("test")
     , code:add_pathz("ebin")
 
-    , etap:plan(1)
+    , etap:plan(7)
     , test()
     , etap:end_tests()
     .
 
 test() -> ok
     , test_api()
+    , test_call_styles()
     .
 
 test_api() -> ok
-    , etap:is(1, 1, "One is one")
+    , Request = request:api()
+    , etap:ok(is_function(Request), "api() returns an API function")
     .
+
+test_call_styles() -> ok
+    , Request = request:api()
+    , ARequest = request:api(async)
+    , {Waiter, Handler} = handler(ok)
+
+    , Res1 = request:req(?COUCH)
+    , etap:ok(is_tuple(Res1), "request:req(url) returns a tuple")
+
+    , Res2 = Request(?COUCH)
+    , etap:ok(is_tuple(Res2), "Request(url) returns a tuple")
+
+    , Pid1 = request:req(?COUCH, Handler)
+    , etap:ok(is_pid(Pid1), "request:req(url, callback) returns a PID")
+    , Waiter(Pid1)
+
+    , Pid2 = ARequest(?COUCH, Handler)
+    , etap:ok(is_pid(Pid2), "Request(url, callback) returns a PID")
+    , Waiter(Pid2)
+    .
+
+handler(ok) -> ok
+    , Waiter_pid = self()
+    , Waiter = fun(Handler_pid) -> ok
+        , receive
+            {Handler_pid, ok} -> ok
+            after ?TIMEOUT    -> throw(timeout)
+            end
+        end
+    , Handler = fun(_Type, _Result) -> ok
+        , etap:ok(true, "Request callback runs")
+        , Waiter_pid ! {self(), ok}
+        end
+    , {Waiter, Handler}
+    .
+
+% vim: sts=4 sw=4 et
